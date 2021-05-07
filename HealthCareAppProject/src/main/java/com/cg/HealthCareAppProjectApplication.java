a4b814f1d43299a978bfbc560a0bf37fb43c26db
afree <afree@DESKTOP-CP73545>
2021-05-07 11:22:34 +0530

    First Commit

---
+++ HealthCareAppProject/src/main/java/com/cg/HealthCareAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareAppProjectApplication.class, args);
+	}
+
+}
